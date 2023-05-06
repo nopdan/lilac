481f042b5cc3ba545c3a4a5ac3a030452c045223
nopdan <me@nopdan.com>
2023-05-07 04:19:52 +0800

    new sample xkjd6

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 func TestBuild(t *testing.T) {
 	// path := "sample/86五笔.ini"
 	// path := "sample/星辰双拼.ini"
-	path := "test/哲豆音形.ini"
+	path := "./sample/星空键道6.ini"
+	// path := "test/哲豆音形.ini"
 	build(path)
 }
